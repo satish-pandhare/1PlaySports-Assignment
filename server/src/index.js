07192b6ee4562c9dfc12a00f10da1b097e385797
Satish Pandhare <satishp120203@gmail.com>
2026-04-09 16:31:34 +0530

    Final README changes

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*',
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
